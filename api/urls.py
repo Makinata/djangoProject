606b33d414d4e25f995a92abc4492e3addcdbd07
Makinata <esfilatov@yandex.ru>
2022-11-19 20:19:13 +0300

    pr11 v1

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('<str:req>', views.Query_broker_View.as_view()),
+    path('info/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'info/docs/',
+        SpectacularSwaggerView.as_view(
+            url_name='schema'
+        ),
+        name='swagger-ui',
+    ),
 ]
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'func_app',
     'rest_framework',
-    'api'
+    'api',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Query broker API',
+    'DESCRIPTION': 'API that throws query to SQLite3 DSBM',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
